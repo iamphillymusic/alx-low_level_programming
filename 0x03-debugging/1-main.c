9060ecdc8d880cceb3f097fabddcc34e5cc6b95b
monday chkwumezirim theophilus <cm.theophilus@gmail.com>
2022-07-05 20:57:56 -0700

    dd

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,11 +4,11 @@ int main(void)
 	int ii;
 	/*length of the array var*/
 	int lenVar = sizeof(var) / sizeof(var[0]);
+
 	for (ii = 0; ii < lenVar - 1; ii++)
 	{
 		_putchar(var[ii]);
 	}
 		_putchar('\n');
-
 	return (0);
-}	
+}
