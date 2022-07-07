3683592e565f7e30cee0e3dcc046a628d3b5cce0
monday chkwumezirim theophilus <cm.theophilus@gmail.com>
2022-07-07 06:12:22 -0700

    task 0

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,14 +1,24 @@
 #include "main.h"
-{
-	char var[] = "_putchar";
-	int ii;
-	/*length of the array var*/
-	int lenVar = sizeof(var) / sizeof(var[0]);
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
 
-	for (ii = 0; ii < lenVar - 1; ii++)
-	{
-		_putchar(var[ii]);
-	}
-		_putchar('\n');
+int main(void)
+{
+	int i;
+
+
+	printf("Infinite loop incoming :(\n");
+	i = 0;
+	/*
+	* 	 * while (i < 10)
+	* 	 	 * {
+	*	putchar(i);
+	* }
+	*/
+
+	printf("Infinite loop avoided! \\o/\n");
+
 	return (0);
 }
