ac8a5efaaeb9a23476efee9666a8de189a5e3f3f
monday chkwumezirim theophilus <cm.theophilus@gmail.com>
2022-07-07 14:41:35 -0700

    Debgging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,4 +1,3 @@
-#include "stdio.h"
 #include "main.h"
 /**
 * largest_number - returns the largest of 3 numbers
@@ -10,20 +9,19 @@
 
 int largest_number(int a, int b, int c)
 {
-	int largest;
+int largest;
 
-	if (a > b && b > c)
-	{
-		largest = a;
-	}
-	else if (b > a && a > c)
-	{
-		largest = b;
-	}
-	else
-	{
-		largest = c;
-	}
+if (a > b && b > c)
+{
+largest = a;
+}
+else if (b > a && a > c)
+{
+largest = b;
+}
+else
+{	largest = c;
+}
 
-	return (largest);
+return (largest);
 }
--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,14 +1,19 @@
 #include "main.h"
-#include "stdio.h"
-
 /**
-* positive_or_negative - Prints 'i is zero' if i is 0,
-* 'i is positive' if i > 0 or 'i is negative' if i < 0
-* @i: The number to check
+* positive_or_negative - prints negative if number is less than 0
+* prints positive if number is greater than 0
+* prints zero if number is 0
+* @i: is the number to be printed
 *
+* Return Nothing
 */
 void positive_or_negative(int i)
 {
-	printf("%d is %s\n", i, i == 0 ? "zero" : i < 0 ? "negative" : "positive");
+if (i > 0)
+printf("%d is positive\n", i);
+else if (i == 0)
+printf("%d is zero\n", i);
+else
+printf("%d is negative\n", i);
+return;
 }
-
