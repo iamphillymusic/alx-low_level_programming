7e14a1e4b2311db000df25feba8d23de50262f4b
monday chkwumezirim theophilus <cm.theophilus@gmail.com>
2022-07-08 08:48:50 -0700

    task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 /**
 * print_diagonal - Draws a diagonal line using the \ character.
 * @n: The number of \ characters to be printed.
@@ -14,7 +14,7 @@ for (space = 0; space < len; space++)
 _putchar(' ');
 _putchar('\\');
 if (len == n - 1)
-	continue;
+continue;
 _putchar('\n');
 }
 }
