cdaf1ca031734b453e7163cbe5ee1aba0a818746
monday chkwumezirim theophilus <cm.theophilus@gmail.com>
2022-07-24 06:18:20 -0700

    I love C

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+* free_grid - free memory dynamically allocated
+* @grid: pointer to 2-D array
+* @row: row of the array
+*
+* Created by: Sanctus-Peter
+* cc: 23rd July, 2022
+*/
+
+void free_grid(int **grid, int row)
+{
+for (i = 0; i < row; i++)
+free(grid[i]);
+free(grid);
+}
